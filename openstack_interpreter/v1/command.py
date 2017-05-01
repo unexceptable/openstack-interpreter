8a6c3523624224b2f1d07e7a50c500ebb19ec7c2
adrian-turjak <adriant@catalyst.net.nz>
2017-05-01 17:08:15 +1200

    Typo fixes
    * also updating changelog

--- openstack_interpreter/v1/command.py
+++ openstack_interpreter/v1/command.py
@@ -31,18 +31,18 @@ class SetupOpenStackInterpreter(command.Command):
                 "It is recommended to set OS_AUTH_URL to be versionless,\n"
                 "and control the identity version with: "
                 "OS_IDENTITY_API_VERSION\n"
-                "If you don't, attempting to use the keystoneclient may\n"
+                "If you don't, attempting to use the keystoneclient may "
                 "throw errors."
                 )
         if "v2" in auth_url:
             output.print_yellow(
                 "WARNING: You are using a deprecated Keystone version.\n"
                 "It is highly recommended that you switch to using v3\n"
-                "for your authentication."
+                "for your authentication.\n"
                 "It is also recommended to set OS_AUTH_URL to be versionless\n"
                 "and control the identity version with: "
                 "OS_IDENTITY_API_VERSION\n"
-                "If you don't, attempting to use the keystoneclient may"
+                "If you don't, attempting to use the keystoneclient may "
                 "throw errors."
                 )
 
